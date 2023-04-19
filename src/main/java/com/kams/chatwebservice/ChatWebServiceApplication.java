73f7f348018057f1c92bfda0d661ca0fe7d74b3d
Kam Lam <kamlam@kams-mbp.mynetworksettings.com>
2023-04-18 22:33:56 -0400

    first commit

---
+++ src/main/java/com/kams/chatwebservice/ChatWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kams.chatwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kams/chatwebservice/ChatWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kams.chatwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
